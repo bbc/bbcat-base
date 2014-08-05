51b749c37c859b731df56ca08a92594422da367a
Richard Day <richard.day@bbc.co.uk>
2014-08-05 09:27:05 +0100

    Add Interpolator and ComplexInterpolator classes and add a MixSamples() function that can apply fading

--- src/SoundMixing.h
+++ src/SoundMixing.h
@@ -1,7 +1,7 @@
 #ifndef __SOUND_MIXING__
 #define __SOUND_MIXING__
 
-#include "misc.h"
+#include "Interpolator.h"
 
 BBC_AUDIOTOOLBOX_START
 
@@ -60,7 +60,31 @@ extern void MixSamples(const Sample_t *src,
                        uint_t nframes = 1,
                        Sample_t mul = 1.0);
 
+/*--------------------------------------------------------------------------------*/
+/** Mix source samples to destination samples (like TransferSamples() but adding instead of over-writing) with level changing
+ *
+ * @param src pointer to source buffer
+ * @param src_channel starting channel to read from
+ * @param src_channels total number of source channels in buffer
+ * @param dst pointer to destination buffer
+ * @param dst_channel starting channel to write to
+ * @param dst_channels total number of destination channels in buffer
+ * @param nchannels number of channels to transfer/convert
+ * @param nframes number of frames to transfer/convert
+ * @param interp level interpolation control
+ * @param inc level interpolation rate
+ *
+ * This function is like the above except that the scaling factor can change linearly using the interp object and inc parameter
+ */
+/*--------------------------------------------------------------------------------*/
+extern void MixSamples(const Sample_t *src,
+                       uint_t src_channel, uint_t src_channels,
+                       Sample_t *dst,
+                       uint_t dst_channel, uint_t dst_channels,
+                       uint_t nchannels,
+                       uint_t nframes,
+                       Interpolator& interp, Sample_t inc);
+
 BBC_AUDIOTOOLBOX_END
 
 #endif
-
